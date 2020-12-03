555d12795ad8d6b587e584b99abc3d70c85c36f6
Rikuto Yamaguchi <yamaguchirkt@gmail.com>
2020-12-03 17:34:09 +0900

    Initialize

---
+++ components/AppButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="onClick"><slot /></button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup(_props, context) {
+    const onClick = () => {
+      context.emit('click')
+    }
+    return {
+      onClick,
+    }
+  },
+})
+</script>
---
+++ pages/sample.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>sample</p>
+    <app-button @click="onClick">app-button</app-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import AppButton from '~/components/AppButton.vue'
+
+export default defineComponent({
+  props: {
+    test: String,
+  },
+  components: {
+    AppButton,
+  },
+  setup() {
+    const onClick = () => {
+      console.log('clicked')
+    }
+    return {
+      onClick,
+    }
+  },
+})
+</script>
